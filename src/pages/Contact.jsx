998b42e51c37fa681816a6625e41128a6a4e5448
MR. Golu <88421428+govaninevil@users.noreply.github.com>
2024-02-26 06:18:54 +0530

    main

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import Header from '../component/Header'
+import Header from '../component/Header';
+import MoveWithCursor from '../component/MoveWithCursor';
+import Shape from '../component/Shape';
+import About from '../component/About';
 
 const Contact = () => {
   return (
     <div>
-      <Header />
+      <MoveWithCursor />
+      <Shape />
+      <div className="md:container mx-auto">
+        <Header />
+        <div className="lg:grid grid-cols-3">
+          <div>
+            <About />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
